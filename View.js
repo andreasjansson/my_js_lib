3649c811d9268e4cb3c5d677ea3a37aca4978670
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2011-07-27 00:48:23 +0100

    added ability to attach extra view actions

--- View.js
+++ View.js
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-View = function(id, scale, clear)
+View = function(id, scale, clear, viewport)
 {
   this.$container = $("#" + id);
   this.scale = scale;
@@ -24,16 +24,23 @@ View = function(id, scale, clear)
     this.clear = true;
   else
     this.clear = clear;
+  if(typeof viewArea == "undefined")
+    this.viewArea = null;
+  else
+    this.viewArea = viewarea;
+
+  this.extraDrawActions = [];
 }
 
 View.prototype.draw = function(elements)
 {
   if(this.clear)
-    this.$container.empty();
+    this.doClear();
   for(var i = 0; i < elements.length; i ++) {
     // make sure element exists
     var element = elements[i];
-    if(element) {
+    if(element &&
+       (!this.viewArea || this.viewArea.contains(element.getPosition))) {
       this.$container.append($("<div style='z-index: " + element.getZIndex() +
                                "; color: " + element.getColour() +
                                "; left: " + (element.getPosition().x *
@@ -43,6 +50,15 @@ View.prototype.draw = function(elements)
                                "px'>" + element.getText() + "</div>"));
     }
   }
+
+  for(var i = 0; i < this.extraDrawActions.length; i ++)
+    if(this.extraDrawActions[i].draw)
+      this.extraDrawActions[i].draw();
+}
+
+View.prototype.doClear = function()
+{
+  this.$container.empty();
 }
 
 View.prototype.destroy = function()
